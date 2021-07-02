79c9e46575c828fd3f08b195a97c3d1bcb6a291b
AmaraliSDJ <amaralimohamed@hotmail.com>
2021-07-02 19:19:57 +0200

    sådan

---
+++ FairytaleLib/src/Core/ViewModelFactory.java
@@ -0,0 +1,18 @@
+package Core;
+
+public class ViewModelFactory {
+
+  // private AddViewModel addViewModel;
+  //private TaskListViewModel taskListViewModel;
+  //private NextTaskViewModel nextTaskViewModel;
+
+  public ViewModelFactory(ModelFactory mf) {
+    //        addViewModel = new AddViewModel(mf.getModel());
+    //    taskListViewModel = new TaskListViewModel(mf.getModel());
+    // nextTaskViewModel = new NextTaskViewModel(mf.getModel());
+  }
+
+  //   public AddViewModel getAddViewModel() {
+  //    return addViewModel;
+  // }
+}
