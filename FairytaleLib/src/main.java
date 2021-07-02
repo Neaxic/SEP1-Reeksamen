55ae70a2b678ac924b8aed6b2cdd5589b4137c94
Andre <andreas79001@gmail.com>
2021-07-02 18:12:09 +0200

    test commit

--- FairytaleLib/src/main.java
+++ FairytaleLib/src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Program started");
+        System.out.println("Program started ");
+        System.out.println("test");
     }
 }
