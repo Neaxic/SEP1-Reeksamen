2decaa4642f57ac390dd039878786bfcdbafabcf
abdullahiisse <abdullahi12351@gmail.com>
2021-07-07 15:16:53 +0200

    problemet med dummy data er blevet fikset

--- FairytaleLib/src/Model/ClientModel.java
+++ FairytaleLib/src/Model/ClientModel.java
@@ -11,11 +11,13 @@ public class ClientModel implements Client
 
 
     public void fillArrayDemoItems(){
-        allProducts.add(new product("DVD", "Ronja Røvedatter", "Ester Erikson", "513BC", "02-03-1990"));
-        allProducts.add(new product("BOG", "Bøjrne brødre", "Bente bent", "A231D", "13-06-2008"));
-        allProducts.add(new product("BOG", "En klog bog", "Aristoteles", "BER31", "16-02-1940"));
-        allProducts.add(new product("CD", "Takwondo banden", "Kent knudsen", "E9182", "21-11-2010"));
-        allProducts.add(new product("ARTIKEL", "Noget reality show", "Tank top thomas", "BDE32", "5-07-1990"));
+        if (allProducts.isEmpty()){
+            allProducts.add(new product("DVD", "Ronja Røvedatter", "Ester Erikson", "513BC", "02-03-1990"));
+            allProducts.add(new product("BOG", "Bøjrne brødre", "Bente bent", "A231D", "13-06-2008"));
+            allProducts.add(new product("BOG", "En klog bog", "Aristoteles", "BER31", "16-02-1940"));
+            allProducts.add(new product("CD", "Takwondo banden", "Kent knudsen", "E9182", "21-11-2010"));
+            allProducts.add(new product("ARTIKEL", "Noget reality show", "Tank top thomas", "BDE32", "5-07-1990"));
+        }
 
     }
 
@@ -31,10 +33,14 @@ public class ClientModel implements Client
 
 
     public ArrayList getAllProductsType(){
-        allProductsType.add("dvd");
-        allProductsType.add("artikel");
-        allProductsType.add("cd");
-        allProductsType.add("BOG");
+        if (allProductsType.isEmpty()){
+
+            allProductsType.add("dvd");
+            allProductsType.add("artikel");
+            allProductsType.add("cd");
+            allProductsType.add("BOG");
+
+        }
 
         return allProductsType;
     }
